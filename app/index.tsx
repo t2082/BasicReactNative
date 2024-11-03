bc383b4f1c0b44feec864975ae9a7d78a328615b
Akuro <luongluuminhtan@gmail.com>
2024-11-03 20:35:08 +0700

    firstpro

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function IndexScreen() {
+    return (<View className="bg-white">
+        <Text> Hello</Text>
+        <Text> Hello</Text>
+        <Text> Hello</Text>
+        <Text> Hello</Text>
+        <Text> Hello</Text>
+    </View>);
+}
